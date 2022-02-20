3f7e33bad1a8a642fe58a9dc894102850571c0ed
Yogesh Bhanushali <bhanushali.yo@gmail.com>
2022-02-21 01:55:46 +0530

    Add project files.

---
+++ CustomMiddleWare/MiddleWare/QueryStringMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomMiddleWare.MiddleWare
+{
+    public class QueryStringMiddleware
+    {
+        private RequestDelegate next;
+
+        public QueryStringMiddleware(RequestDelegate _next)
+        {
+            next = _next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Method == HttpMethods.Get && context.Request.Query["fname"] == "Yogesh")
+            {
+                await context.Response.WriteAsync("Message from class Based Middleware");
+
+            }
+            await next(context);
+        }
+        
+    }
+}
